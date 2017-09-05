1567cc0dbdcc0c6cbc684960bf658258763b412f
Andrey Bulgakov <mail@andreiko.ru>
2017-09-05 15:02:11 -0700

    Added support for scanning in chunks

--- driver/base.go
+++ driver/base.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/jmoiron/sqlx"
 	"github.com/segment-sources/sqlsource/domain"
 	"github.com/segmentio/go-snakecase"
 	"github.com/segmentio/objects-go"
@@ -14,8 +13,15 @@ import (
 type Driver interface {
 	Init(*domain.Config) error
 	Describe() (*domain.Description, error)
-	Scan(t *domain.Table) (*sqlx.Rows, error)
+	Scan(t *domain.Table, afterPKValues []interface{}) (SqlRows, error)
 	Transform(row map[string]interface{}) map[string]interface{}
+}
+
+type SqlRows interface {
+	Next() bool
+	MapScan(map[string]interface{}) error
+	Err() error
+	Close() error
 }
 
 type Base struct {
@@ -23,20 +29,46 @@ type Base struct {
 }
 
 func (b *Base) ScanTable(t *domain.Table, publisher domain.ObjectPublisher) error {
-	rows, err := b.Driver.Scan(t)
+	var lastPkValues []interface{}
+
+	for {
+		var err error
+		lastPkValues, err = b.scanTableChunk(t, lastPkValues, publisher)
+
+		if err != nil {
+			return err
+		}
+
+		if lastPkValues == nil {
+			return nil
+		}
+	}
+}
+
+// scanTableChunk performs Scan operation on the driver and returns values of primary keys from the last row or an empty
+// array if no rows were returned from the driver
+func (b *Base) scanTableChunk(t *domain.Table, afterPKValues []interface{}, publisher domain.ObjectPublisher) ([]interface{}, error) {
+	rows, err := b.Driver.Scan(t, afterPKValues)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer rows.Close()
 
+	lastPkValues := make([]interface{}, len(t.PrimaryKeys))
+	rowsFound := false
 	for rows.Next() {
+		rowsFound = true
 		row := map[string]interface{}{}
 		if err := rows.MapScan(row); err != nil {
-			return err
+			return nil, err
 		}
 		log.WithFields(log.Fields{"row": row, "table": t.TableName, "schema": t.SchemaName}).Debugf("Received Row")
 		t.IncrScanned()
+
+		for i, p := range t.PrimaryKeys {
+			lastPkValues[i] = row[p]
+		}
 
 		row = b.Driver.Transform(row)
 		pks := []string{}
@@ -52,8 +84,12 @@ func (b *Base) ScanTable(t *domain.Table, publisher domain.ObjectPublisher) erro
 	}
 
 	if err := rows.Err(); err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	if !rowsFound {
+		return nil, nil
+	}
+
+	return lastPkValues, nil
 }
